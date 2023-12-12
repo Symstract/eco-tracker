57f91240853ad559383a65e747b17bc17a7c4bb0
Antti Tikka <antti.tikka@hotmail.com>
2023-12-12 19:57:12 +0200

    Add placeholder routes

--- frontend/app/home/index.js
+++ frontend/app/home/index.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export default function Home() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
